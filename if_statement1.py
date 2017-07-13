81044415c00633d9b36facd010be93fa0c2ff8c1
Kevin Xin <kevinsxin@126.com>
2017-07-13 10:58:26 +0800

    Basic if statement examples, showing equality and inequalities with strings and integers.

---
+++ if_statement1.py
@@ -0,0 +1,54 @@
+# Copy and paste this code and run it while looking at this code!
+
+raw_input('Part 1')
+
+bread = 'tasty'
+
+
+if bread == 'tasty': # Checks if bread is equal to tasty.
+    print 'The bread is tasty!'
+else: # If not equal.
+    print 'The bread is NOT tasty.'
+
+
+print 'the value of bread is: ' + bread
+print "bread == 'tasty' evaluates to " + str(bread == 'tasty')
+
+raw_input('Part 2')
+
+bread = 'disgusting'
+
+if bread == 'tasty':
+    print 'The bread is tasty!'
+else:
+    print 'The bread is NOT tasty.'
+
+print 'the value of bread is: ' + bread
+print "bread == 'tasty' evaluates to " + str(bread == 'tasty')
+
+raw_input('Part 3')
+
+bread = 'okay'
+
+if bread == 'tasty': # Goes first.
+    print 'The bread is tasty!'
+elif bread == 'okay': # Else if the bread is okay.
+    print 'The bread is meh.'
+else:
+    pass # Placeholder to avoid error, does nothing
+
+print 'the value of bread is: ' + bread
+print "bread == 'tasty' evaluates to " + str(bread == 'tasty')
+print "bread == 'okay' evaluates to " + str(bread == 'okay')
+
+raw_input('Part 4')
+
+bread = 'okay'
+
+if bread != 'okay': # != is does not equal.
+    print 'This bread is not okay.'
+else:
+    print 'This bread is okay.'
+
+print 'the value of bread is: ' + bread
+print "bread != 'okay' evaluates to " + str(bread == 'okay')
---
+++ if_statement2.py
@@ -0,0 +1,16 @@
+# Copy and paste this code, try changing my number to see the output!
+
+raw_input('Part 1')
+
+my_number = 5
+
+if my_number < 0: # Checks if bread is equal to tasty.
+    print 'My number is smaller than 0.'
+elif my_number <= 0:
+    print 'My number is smaller OR equal to 0.'
+elif my_number > 5:
+    print 'My number is larger than 5.'
+elif my_number >= 5:
+    print 'My number is larger OR equal to 5.'
+else:
+    print 'My number is between 1 and 4.'
